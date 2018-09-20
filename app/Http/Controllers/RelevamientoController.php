a5985cbea02e9f821093f95c0e3b05482d7eb879
Camila Blanc <camilablancfick@gmail.com>
2018-09-20 08:54:07 -0300

    fsfv

--- app/Http/Controllers/RelevamientoController.php
+++ app/Http/Controllers/RelevamientoController.php
@@ -1076,11 +1076,11 @@ class RelevamientoController extends Controller
                      ->leftJoin('tipo_causa_no_toma','tipo_causa_no_toma.id_tipo_causa_no_toma','=','detalle_relevamiento.id_tipo_causa_no_toma')
                      ->join('usuario','usuario.id_usuario','=','relevamiento.id_usuario_cargador')
                      ->where('maquina.id_maquina',$maquina->id_maquina)
-                     ->groupby('relevamiento.id_relevamiento',
+                     //->groupby()
+                     ->distinct('relevamiento.id_relevamiento',
                                'detalle_relevamiento.id_detalle_relevamiento',
                                'usuario.id_usuario',
                                'detalle_contador_horario.id_detalle_contador_horario')
-                     ->distinct()
                      ->orderBy('relevamiento.fecha','desc')
                      ->take(5)->get();
     return ['maquina' => $maq,
