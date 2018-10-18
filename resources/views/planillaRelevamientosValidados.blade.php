1d4eef0fab2f067d7056c0f4c7027427aecb9f34
mariano <mariano24.palacios@gmail.com>
2018-10-18 09:25:22 -0300

    cambio de nombre

--- resources/views/planillaRelevamientosValidados.blade.php
+++ resources/views/planillaRelevamientosValidados.blade.php
@@ -55,7 +55,7 @@ div.breakNow { page-break-inside:avoid; page-break-after:always; }
 
         <div class="encabezadoImg">
               <img src="img/logos/banner_loteria_landscape2_f.png" width="900">
-              <h2><span style="color: #9c9c9c; ">PMTM04 | Relevamiento diario de contadores de máquinas tragamonedas (MTM) - {{$rel->casinoCod}}</span></h2>
+              <h2><span style="color: #9c9c9c; ">RMTM04 | Relevamiento diario de contadores de máquinas tragamonedas (MTM) - {{$rel->casinoCod}}</span></h2>
         </div>
         <div class="primerEncabezado" style="padding-left: 120px;">Datos del relevamiento para la fecha de producción - <b><i><?php echo $fecha_final;?> </i></b></div><br>
                   <div style="padding-left: 65px;">
