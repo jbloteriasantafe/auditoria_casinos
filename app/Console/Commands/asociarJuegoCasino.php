e99763c740c50b872ff6165cf5fbafec4a876813
mariano <mariano24.palacios@gmail.com>
2018-11-28 11:53:43 -0300

    se crea en el metodo la tabla

--- app/Console/Commands/asociarJuegoCasino.php
+++ app/Console/Commands/asociarJuegoCasino.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Juego;
 use App\Maquina;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 class asociarJuegoCasino extends Command
@@ -41,13 +42,16 @@ class asociarJuegoCasino extends Command
     public function handle()
     {
         $cant=0;
+        // primero limipio la tabla y la vuelvo a crear
+        $this->dropAndCreateTable();
+        
         foreach(Juego::all()as $juego){
             
             $casinosAsignados=$this->casinos($juego->id_juego);
             //echo "\n el juego con id: " , $juego->id_juego , " tiene los casinos ";
             //print_r($casinosAsignados);
             if(sizeof($casinosAsignados)!=0){
-                $cant=$cant+1;
+                $cant=$cant+sizeof($casinosAsignados);
                 //$juego->casinos()->attach($casinosAsignados);
             }
         }
@@ -68,6 +72,18 @@ class asociarJuegoCasino extends Command
         return $casinos;
     }
 
+    private function dropAndCreateTable(){
+        Schema::dropIfExists('casino_tiene_juego');
+        Schema::create('casino_tiene_juego', function($table)
+        {
+            $table->increments('id_casino_tiene_juego');
+            $table->integer('id_casino');
+            $table->integer('id_juego');
+            $table->foreign('id_casino')->references('id_casino')->on('casino');
+            $table->foreign('id_juego')->references('id_juego')->on('juego');
+        });
+
+    }
     private function casinoSantaFe($id_juego) {
         $maquinasID=DB::table('maquina_tiene_juego')
             ->where('id_juego',$id_juego)
