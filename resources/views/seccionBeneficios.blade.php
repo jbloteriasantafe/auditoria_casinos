7789c717021bd7250013c31790b6761868717c83
mariano <mariano24.palacios@gmail.com>
2018-12-18 10:48:23 -0300

    se valida si tiene permisos

--- resources/views/seccionBeneficios.blade.php
+++ resources/views/seccionBeneficios.blade.php
@@ -5,8 +5,11 @@
 @section('contenidoVista')
 <?php
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 setlocale(LC_TIME, 'es_ES.UTF-8');
+$usuario = UsuarioController::getInstancia()->buscarUsuario(session('id_usuario'));
+$id_usuario = $usuario['usuario']->id_usuario;
 
 ?>
 
@@ -149,7 +152,7 @@ setlocale(LC_TIME, 'es_ES.UTF-8');
                   </a>
                 </div>
               </div>
-
+              @if(AuthenticationController::getInstancia()->usuarioTienePermiso($id_usuario,'cotizar_dolar_peso'))  
               <div class="row">
                 <div class="col-md-12">
                   <a id="btn-cotizacion" href="" style="text-decoration:none;">
@@ -161,7 +164,7 @@ setlocale(LC_TIME, 'es_ES.UTF-8');
                   </a>
                 </div>
               </div>
-
+              @endif
             </div> <!-- /.col-md-3 -->
 
         </div> <!-- /.row -->
@@ -312,33 +315,33 @@ setlocale(LC_TIME, 'es_ES.UTF-8');
 
     
     <!-- Modal cotizacion -->
-        
-    <div class="modal fade" id="modal-cotizacion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog" style="width: 60%" >
-            <div class="modal-header modalNuevo" style="font-family: Roboto-Black; background-color: #6dc7be; color: #fff">
-                <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> -->
-                <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
-                <button id="btn-minimizar" type="button" class="close" data-toggle="collapse" data-minimizar="true" data-target="#colapsado" style="position:relative; right:20px; top:5px"><i class="fa fa-minus"></i></button>
-                <h3 class="modal-title">| COTIZACIÓN DÓLAR->PESO</h3>
-               </div>
+      <div class="modal fade" id="modal-cotizacion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog" style="width: 60%" >
+              <div class="modal-header modalNuevo" style="font-family: Roboto-Black; background-color: #6dc7be; color: #fff">
+                  <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button> -->
+                  <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
+                  <button id="btn-minimizar" type="button" class="close" data-toggle="collapse" data-minimizar="true" data-target="#colapsado" style="position:relative; right:20px; top:5px"><i class="fa fa-minus"></i></button>
+                  <h3 class="modal-title">| COTIZACIÓN DÓLAR->PESO</h3>
+                </div>
 
-               <div class="modal-body modalCuerpo" style="background-color: white;">
+                <div class="modal-body modalCuerpo" style="background-color: white;">
 
-                  <div class="row" style="padding-bottom: 15px;">
-                      <div class="col-md-12">
-                          <div id="calendarioInicioBeneficio"></div>
-                    </div>
+                    <div class="row" style="padding-bottom: 15px;">
+                        <div class="col-md-12">
+                            <div id="calendarioInicioBeneficio"></div>
+                      </div>
 
-              </div>
+                </div>
 
-               <div class="modal-footer">
-                <label id="labelCotizacion" for="number"> </label>
-                <input id="valorCotizacion" type="number" step="0.001" min="25" max="200">
-                <button type="button" class="btn btn-successAceptar" id="guardarCotizacion">GUARDAR</button>
-              </div> 
+                <div class="modal-footer">
+                  <label id="labelCotizacion" for="number"> </label>
+                  <input id="valorCotizacion" type="number" step="0.001" min="25" max="200">
+                  <button type="button" class="btn btn-successAceptar" id="guardarCotizacion">GUARDAR</button>
+                </div> 
+            </div>
           </div>
-        </div>
-  </div>
+      </div>
+    
 
 
     <meta name="_token" content="{!! csrf_token() !!}" />
