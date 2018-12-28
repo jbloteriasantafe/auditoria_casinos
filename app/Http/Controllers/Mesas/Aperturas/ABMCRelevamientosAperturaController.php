dce6afa80e64f7c45cf61b49836b548002a5b4e2
Camila Blanc <camilablancfick@gmail.com>
2018-12-28 12:31:42 -0300

    f

--- app/Http/Controllers/Mesas/Aperturas/ABMCRelevamientosAperturaController.php
+++ app/Http/Controllers/Mesas/Aperturas/ABMCRelevamientosAperturaController.php
@@ -281,13 +281,13 @@ class ABMCRelevamientosAperturaController extends Controller
       $contador = 1;
       foreach ($m_ordenadas as $m) {
         if($contador == 20){ //20 = cant de mesas que entran en de 1
-          $sublista[] = ['codigo_mesa'=> $m->nro_mesa]
+          $sublista[] = ['codigo_mesa'=> $m->nro_mesa];
 
           $lista_mesas[] = $sublista;
           $sublista = array();
           $contador = 1;
         }else{
-          $sublista[] = ['codigo_mesa'=> $m->nro_mesa]
+          $sublista[] = ['codigo_mesa'=> $m->nro_mesa];
 
           $contador++;
         }
