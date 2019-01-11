7f183fe04bb9d86e71e6ab2a9d355b1a54327224
Camila Blanc <camilablancfick@gmail.com>
2019-01-11 11:50:38 -0300

    view cierres ap

--- resources/views/CierresAperturas/CierresAperturas.blade.php
+++ resources/views/CierresAperturas/CierresAperturas.blade.php
@@ -175,7 +175,6 @@ $cas = $usuario['usuario']->casinos;
 
           <!-- TABLA -->
           <div class="row">
-              <div class="col-md-12">
                 <div class="panel panel-default">
                   <div class="panel-heading">
                     <h4 id="tablaInicial">APERTURAS</h4>
@@ -273,7 +272,6 @@ $cas = $usuario['usuario']->casinos;
 
                     </div>
                   </div>
-                </div>
           </div>
     </div> <!-- .row / TABLA -->
 
