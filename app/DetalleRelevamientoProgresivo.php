62557b9ffcaa8a0c755b94a5c68bb90ce9415204
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2019-08-28 10:43:05 -0300

    Tipo causa no toma progresivo

--- app/DetalleRelevamientoProgresivo.php
+++ app/DetalleRelevamientoProgresivo.php
@@ -9,7 +9,7 @@ class DetalleRelevamientoProgresivo extends Model
   protected $connection = 'mysql';
   protected $table = 'detalle_relevamiento_progresivo';
   protected $primaryKey = 'id_detalle_relevamiento_progresivo';
-  protected $visible = array('id_detalle_relevamiento_progresivo', 'id_relevamiento_progresivo','id_pozo', 'nivel1', 'nivel2', 'nivel3', 'nivel4','nivel5','nivel6');
+  protected $visible = array('id_detalle_relevamiento_progresivo', 'id_relevamiento_progresivo','id_pozo','id_tipo_causa_no_toma_progresivo', 'nivel1', 'nivel2', 'nivel3', 'nivel4','nivel5','nivel6');
   public $timestamps = false;
 
   public function relevamiento_progresivo(){
@@ -18,5 +18,9 @@ class DetalleRelevamientoProgresivo extends Model
 
   public function pozo(){
     return $this->belongsTo('App\Pozo','id_pozo','id_pozo');
+  }
+
+  public function tipo_causa_no_toma(){
+    return $this->belongsTo('App\TipoCausaNoTomaProgresivo','id_tipo_causa_no_toma_progresivo','id_tipo_causa_no_toma_progresivo');
   }
 }
