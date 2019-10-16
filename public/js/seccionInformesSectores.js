48344f160d540485f681bff9e12ca8dedc798993
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2019-10-16 11:28:45 -0300

    Informes sector: Fix estaba mostrando maquinas de una isla vacia porque no se limpiaba correctamente

--- public/js/seccionInformesSectores.js
+++ public/js/seccionInformesSectores.js
@@ -7,18 +7,27 @@ $(document).ready(function(){
 $('#sel_casinos').change(function(){
     const id_casino = $(this).val();
     const sectores = $('#dataSectores option[data-id-casino="'+id_casino+'"]').clone();
+    let sel_sectores = $('#sel_sectores');
+    sel_sectores.empty();
     if(sectores.length > 0) sectores[0].selected=true;
-    $('#sel_sectores').empty().append(sectores).change();
+    sel_sectores.append(sectores);
+    sel_sectores.change();
 });
 $('#sel_sectores').change(function(){
     const id_sector = $(this).val();
     const islas = $('#dataIslas option[data-id-sector="'+id_sector+'"]').clone(); 
+    let sel_islas = $('#sel_islas');
+    sel_islas.empty();
     if(islas.length > 0) islas[0].selected=true;
-    $('#sel_islas').empty().append(islas).change();
+    sel_islas.append(islas);
+    sel_islas.change();
 });
 $('#sel_islas').change(function(){
     const id_isla = $(this).val();
     const maquinas = $('#dataMaquinas option[data-id-isla="'+id_isla+'"]').clone();
+    let sel_maquinas = $('#sel_maquinas');
+    sel_maquinas.empty();
     if(maquinas.length > 0) maquinas[0].selected=true;
-    $('#sel_maquinas').empty().append(maquinas).change();
+    sel_maquinas.append(maquinas);
+    sel_maquinas.change();
 })
