35123b15ca6daf3b968e39a109b281e3b02aa1f5
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2022-06-14 13:11:43 -0300

    Ammend

--- resources/views/includes/barraMenuPrincipal_texto_con_icono.blade.php
+++ resources/views/includes/barraMenuPrincipal_texto_con_icono.blade.php
@@ -1,11 +1,11 @@
 <div class="texto_con_icono">
   @if(isset($icono) && $icono != '')
-  <span class="texto_con_icono_icono" style="flex: 1;">
+  <span style="flex: 1;">
   {!! $icono !!}
   </span>
   @endif
   @if(isset($op) && $op != '')
-  <span class="texto_con_icono_texto" style="flex: 4;">
+  <span style="flex: 4;">
   {!! $op !!}
   </span>
   @endif
