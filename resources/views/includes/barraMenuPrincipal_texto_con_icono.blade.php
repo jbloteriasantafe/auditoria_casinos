bd114fa24c50cae0132681d4ceb00a62147d6759
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2022-06-14 13:10:10 -0300

    Menu Desplegable: Diferenciar un poco los submenues de los enlaces

--- resources/views/includes/barraMenuPrincipal_texto_con_icono.blade.php
+++ resources/views/includes/barraMenuPrincipal_texto_con_icono.blade.php
@@ -1,11 +1,11 @@
 <div class="texto_con_icono">
   @if(isset($icono) && $icono != '')
-  <span style="flex: 1;">
+  <span class="texto_con_icono_icono" style="flex: 1;">
   {!! $icono !!}
   </span>
   @endif
   @if(isset($op) && $op != '')
-  <span style="flex: 4;">
+  <span class="texto_con_icono_texto" style="flex: 4;">
   {!! $op !!}
   </span>
   @endif
