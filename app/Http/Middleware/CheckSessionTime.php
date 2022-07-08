2f2aa7e7f5c06fa51ce9573bdf532bbf258cb4c0
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2022-07-08 12:45:18 -0300

    FIX En el chequeo incorrecto de tiempo de sesión

--- app/Http/Middleware/CheckSessionTime.php
+++ app/Http/Middleware/CheckSessionTime.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use App\Http\Controllers\AuthenticationController;
 
 class CheckSessionTime
@@ -15,28 +14,29 @@ class CheckSessionTime
      * @param  \Closure  $next
      * @return mixed
      */
-    protected $tiempo_sesion = 14400;// segundos
+    protected $tiempo_sesion = 14400;//4 horas en segundos
 
     public function handle($request, Closure $next)
     {
       //Si accede desde la API no chequeo el tiempo
       if(!empty($request->header('API-Token'))) return $next($request);
 
-      $ahora = date_create();
+      $ahora = time();
       $ultima_actividad = $request->session()->has('last_activity_time') ? $request->session()->get('last_activity_time') : null;
-      if($ultima_actividad != null && date_diff($ahora,$ultima_actividad)->format('%s') > $this->tiempo_sesion && $request->path() != 'login'){
+      $ultima_actividad = $ultima_actividad === null? ($ahora+$this->tiempo_sesion+1) : $ultima_actividad->getTimestamp();
+      $diferencia = $ahora-$ultima_actividad;
+      if($diferencia > $this->tiempo_sesion && $request->path() != 'login'){
         AuthenticationController::getInstancia()->logout($request);
         $request->session()->put('redirect',$request->path());
         if($request->ajax()){
           return response()->json(['mensaje' => 'Su sesión ha expirado, ingrese de nuevo al sistema','url' => '/'],351,[['Content-Type', 'application/json']]);
         }
-        else{
-          return redirect('/');
-        }
+        return redirect('/');
       }
-      else{ // se actualiza el tiempo de la ultima actividad
-        $request->session()->put('last_activity_time',$ahora);
-        return $next($request);
-      }
+      // se actualiza el tiempo de la ultima actividad
+      $ahora_datetime = new \DateTime();
+      $ahora_datetime->setTimestamp($ahora);
+      $request->session()->put('last_activity_time',$ahora_datetime);
+      return $next($request);
     }
 }
