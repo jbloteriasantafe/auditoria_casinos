0f0db25430077e836b4dfbce25de6135326592c6
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2022-12-28 09:44:13 -0300

    Renombrar prefix de glihards a certificadoHard, mover API calls donde se usan, poner permiso para todas las calls

--- resources/views/seccionMaquinas.blade.php
+++ resources/views/seccionMaquinas.blade.php
@@ -835,7 +835,7 @@ $usuario = UsuarioController::getInstancia()->quienSoy();
     <script src="/js/seccionMaquinas-Formula.js" charset="utf-8"></script>
     <script src="/js/seccionMaquinas-JuegoNuevo.js?3" charset="utf-8"></script>
     <script src="/js/seccionMaquinas-GliSoftNuevo.js" charset="utf-8"></script>
-    <script src="/js/seccionMaquinas-GliHardNuevo.js" charset="utf-8"></script>
+    <script src="/js/seccionMaquinas-GliHardNuevo.js?1" charset="utf-8"></script>
     <script src="/js/seccionMaquina-IslaNuevo.js" charset="utf-8"></script>
     <script src="/js/seccionMaquinas-Modal.js?1" charset="utf-8"></script>
     <script src="/js/seccionMaquinas.js" charset="utf-8"></script>
