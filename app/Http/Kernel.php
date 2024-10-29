c927ee1dea8ddd43d66c394deac897929deb04d8
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2024-10-29 09:26:13 -0300

    Kernel: agregar funcionalidad para chequear roles

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,6 @@ class Kernel extends HttpKernel
         'tiene_permiso' => \App\Http\Middleware\CheckPermission::class,
         'tiene_algun_permiso' => \App\Http\Middleware\CheckAnyPermission::class,
         'check_API_token' => \App\Http\Middleware\CheckAPIToken::class,
+        'tiene_rol' => \App\Http\Middleware\CheckRole::class,
     ];
 }
