5e006bd1c65157f1b52cdd999a0db92903cb2709
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2024-11-13 12:49:06 -0300

    Revert "ammend agregar ajuste"
    This reverts commit b65953bb5939eccb01b9eea86597c34a914c538e.

--- app/Http/Controllers/CanonController.php
+++ app/Http/Controllers/CanonController.php
@@ -1612,7 +1612,7 @@ class CanonController extends Controller
       $max_scale = 2;//Sumo usando la maxima escala posible...
       foreach($conceptos as $concepto => $matcheables){     
         $acumulado = ($concepto == 'Total Físico' && $tipo_presupuesto == 'determinado')?
-          $canons_casino['ajuste']
+          $canons['ajuste']
         : null;
         foreach($matcheables as $matcheable){
           foreach($canons_casino as $tipo => $canons_casino_tipo){ foreach($canons_casino_tipo as $canon_casino_subtipo){
