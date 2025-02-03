2d60f404a83c261be2deb22bb19796ff1b3e42a3
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2025-02-03 11:38:59 -0300

    Canon: FIX cambio de nombre de atributo

--- app/Http/Controllers/CanonController.php
+++ app/Http/Controllers/CanonController.php
@@ -2305,7 +2305,7 @@ class CanonController extends Controller
         }
         $fila[$tval] = ($t['Total'] ?? [])[$tval] ?? '0';
       }
-      $fila['intereses'] = self::formatear_decimal($d->intereses);
+      $fila['intereses_y_cargos'] = self::formatear_decimal($d->intereses_y_cargos);
       $fila['pago']      = self::formatear_decimal($d->pago);
       $fila['saldo_posterior'] = self::formatear_decimal($d->saldo_posterior);
       $arreglo_a_csv[] = $fila;
