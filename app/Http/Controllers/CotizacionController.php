3f5a4ea5b05505a111b9873fe8e873859fec2a6a
Octavio Garcia Aguirre <octaviogarciaaguirre@gmail.com>
2026-06-18 12:15:23 -0300

    Cotización: updatear para estar compatible con BNA

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -56,6 +56,7 @@ class CotizacionController extends Controller
       
       $CACHE_CODIGO = 'cotizacionesBNA';
       $CACHE_SUBCODIGO = $año_mes;
+      //$CC->invalidar($CACHE_CODIGO,$CACHE_SUBCODIGO);
       //Dentro de los ultimos 30 minutos
       $cache = $CC->buscarUltimoDentroDeSegundos($CACHE_CODIGO,$CACHE_SUBCODIGO,60*30);
       if(!is_null($cache)){
@@ -68,44 +69,72 @@ class CotizacionController extends Controller
         },array_reverse(explode('-',$f))));
       };
       
-      $responses = [];
-      foreach(['dolar' => 'filtroDolarDescarga=1','euro' => 'filtroEuroDescarga=1'] as $moneda => $param_moneda){
-        $params = http_build_query([
-          'fechaDesde' => $ISO_a_barras($fechaDesde),
-          'fechaHasta' => $ISO_a_barras($fechaHasta)
-        ]);
-        
-        set_time_limit(5);
-        $ch = curl_init();
-        $url = "https://www.bna.com.ar/Cotizador/DescargarPorFecha";
-        $url .= '?'.$params.'&'.$param_moneda;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-        $proxy_url  = env('HTTP_PROXY_URL',null);
-        $proxy_port = env('HTTP_PROXY_PORT',null);
-        if($proxy_url !== null && $proxy_port !== null){
-          curl_setopt($ch, CURLOPT_PROXY, $proxy_url);
-          curl_setopt($ch, CURLOPT_PROXYPORT, $proxy_port);
-        }
-        
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-          'Content-Type: application/x-www-form-urlencoded'
-        ]);
-        $response = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        
+      $responses = [];//Originalmente se hacia en varios requests... devuelvo un array de length 1
+      
+      $monedas = [//id en BNA
+        'Dolar U.S.A' => 22,
+        'Euro' => 12,
+        'Real (*)' => 23
+      ];
+      $monedas_front = [
+        'Dolar U.S.A' => 'dolar',
+        'Euro' => 'euro',
+        'Real (*)' => 'real',
+      ];
+      
+      $url = "https://www.bna.com.ar/Cotizador/DescargarPorFecha?";
+      
+      $url.= http_build_query([
+        'fechaDesde' => $ISO_a_barras($fechaDesde),
+        'fechaHasta' => $ISO_a_barras($fechaHasta),
+        'id' => 'billetes'
+      ]);
+      
+      foreach($monedas as $nombre => $id){
+        $url.='&idMonedaDescarga='.$id;//Esto no es estandar... deberia ser idMonedaDescarga[]= , pero bueno
+      }
+      
+      set_time_limit(5);
+      $ch = curl_init();
+      
+      curl_setopt($ch, CURLOPT_URL, $url);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+      curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+      curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language: es-AR,es;q=0.9,en;q=0.8',
+        'Connection: keep-alive',
+        'Upgrade-Insecure-Requests: 1'
+      ]);
+      $proxy_url  = env('HTTP_PROXY_URL',null);
+      $proxy_port = env('HTTP_PROXY_PORT',null);
+      if($proxy_url !== null && $proxy_port !== null){
+        curl_setopt($ch, CURLOPT_PROXY, $proxy_url);
+        curl_setopt($ch, CURLOPT_PROXYPORT, $proxy_port);
+      }
+      
+      curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/x-www-form-urlencoded'
+      ]);
+      $response = curl_exec($ch);
+      $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      
+      do {
         if(curl_errno($ch)){
-          $responses[$moneda] = [500,['CURL_ERROR' => [curl_error($ch)]]];
+          $responses[0] = [500,['CURL_ERROR' => [curl_error($ch)]]];
           curl_close($ch);
-          continue;
+          break;
         }
         else if($httpCode != 200){
-          $responses[$moneda] = [$httpCode,['RESPONSE_ERROR' => [$httpCode,$response]]];
+          $responses[0] = [$httpCode,['RESPONSE_ERROR' => [$httpCode,$response]]];
           curl_close($ch);
-          continue;
+          break;
         }
         curl_close($ch);
+        
         $response = str_replace("\r\n","\n",$response);
         $body = [];
         $header = [];
@@ -135,7 +164,6 @@ class CotizacionController extends Controller
         }
         
         $a_eliminar = [];
-        
         foreach($header as $hidx => $h){
           $columna_vacia = empty($h);
           if(!$columna_vacia) continue;
@@ -146,7 +174,6 @@ class CotizacionController extends Controller
           //Toda la columna vacia
           $a_eliminar[] = $hidx;
         }
-        
         foreach($a_eliminar as &$hidx){
           unset($header[$hidx]);
           foreach($body as $lidx => $line){
@@ -155,19 +182,28 @@ class CotizacionController extends Controller
           }
         }
         
+        
         $header = array_values($header);
         foreach($body as $lidx => $line){
           $body[$lidx] = array_values($line);
         }
         
-        if($header != ['Fecha cotizacion','Compra','Venta']){
-          $responses[$moneda] = [500,['CODE_ERROR' => ['Header inesperado',implode(',',$header)]]];
-          continue;
+        if($header != ['Moneda','Fecha cotizacion','Compra','Venta']){
+          $responses[0] = [500,['CODE_ERROR' => ['Header inesperado',implode(';',$header)]]];
+          break;
         }
         
-        $responses[$moneda] = [200,compact('header','body')];
-      }
-      
+        $header_idx = array_flip($header);
+        foreach($monedas_front as $mcsv => $mfront){
+          foreach($body as $row){
+            $mrow = $row[$header_idx['Moneda']];
+            if($mrow != $mcsv) continue;
+            $responses[$mfront] = $responses[$mfront] ?? [200,['header' => $header,'body' => []]];
+            $responses[$mfront][1]['body'][] = $row;
+          }
+        }
+      } while(false);
+                  
       $con_error = array_filter($responses,function($r){
         return $r[0] != 200;
       });
@@ -176,20 +212,20 @@ class CotizacionController extends Controller
       }
       
       $merged = [];
-      foreach($responses as $moneda => $code_headerbody){
+      foreach($responses as $mfront => $code_headerbody){
         $headerbody = $code_headerbody[1];
         $header_idx = array_flip($headerbody['header']);
         foreach($headerbody['body'] as $row){
-          $fecha_cotizacion = $row[$header_idx['Fecha cotizacion']];
+          $fecha_cotizacion = trim($row[$header_idx['Fecha cotizacion']]);
           $yyyymmdd = array_reverse(explode('/',$fecha_cotizacion));
           $yyyymmdd[0] = str_pad($yyyymmdd[0],4,'0',STR_PAD_LEFT);
           $yyyymmdd[1] = str_pad($yyyymmdd[1],2,'0',STR_PAD_LEFT);
           $yyyymmdd[2] = str_pad($yyyymmdd[2],2,'0',STR_PAD_LEFT);
           $fecha_cotizacion = implode('-',$yyyymmdd);
           $merged[$fecha_cotizacion] = $merged[$fecha_cotizacion] ?? [];
-          $compra = str_replace('.',',',$row[$header_idx['Compra']]);
-          $venta = str_replace('.',',',$row[$header_idx['Venta']]);
-          $merged[$fecha_cotizacion][$moneda] = compact('compra','venta');          
+          $compra = trim(str_replace('.',',',$row[$header_idx['Compra']]));
+          $venta = trim(str_replace('.',',',$row[$header_idx['Venta']]));
+          $merged[$fecha_cotizacion][$mfront] = compact('compra','venta');          
         }
       }
       
